5d208a362af151be6d31ca498d80f6f54a2d2c81
César Guerra <warderer@gmail.com>
2023-03-06 20:41:37 -0600

    ✨ Primer componente HolaMundo

---
+++ 01-vite-vanilla/src/components/HolaMundo.js
@@ -0,0 +1,5 @@
+const HolaMundo = () => `
+    <h1>Hola Mundo Vite JS Vanilla</h1>
+    <p>Contenido desde template string</p>
+`
+export default HolaMundo
--- 01-vite-vanilla/src/main.js
+++ 01-vite-vanilla/src/main.js
@@ -1,21 +1,8 @@
 import './styles/style.css'
 import javascriptLogo from './assets/javascript.svg'
+import HolaMundo from './components/HolaMundo'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
+  ${HolaMundo()}
 `
 
