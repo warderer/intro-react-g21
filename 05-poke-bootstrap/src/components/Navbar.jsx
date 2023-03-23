aeb35e0c967649b6f523e3b41ed63352b07b2191
César Guerra <warderer@gmail.com>
2023-03-22 20:09:06 -0600

    ✨ navbar y busqueda de pokemons

--- 05-poke-bootstrap/src/components/Navbar.jsx
+++ 05-poke-bootstrap/src/components/Navbar.jsx
@@ -3,12 +3,12 @@ const Navbar = () => {
     <nav className='navbar navbar-expand-sm navbar-light bg-warning d-flex justify-content-between px-4'>
       <a className='navbar-brand' href='#'>Pokédex</a>
 
-      <ul class='navbar-nav mr-auto mt-2 mt-lg-0'>
-        <li class='nav-item px-2'>
-          <a class='nav-link' href='/'>Home</a>
+      <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+        <li className='nav-item px-2'>
+          <a className='nav-link' href='/'>Home</a>
         </li>
-        <li class='nav-item px-2'>
-          <a class='nav-link' href='/about'>Acerca de Pokémon</a>
+        <li className='nav-item px-2'>
+          <a className='nav-link' href='/about'>Acerca de Pokémon</a>
         </li>
       </ul>
 
